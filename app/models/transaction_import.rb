bd548a1675f98dd1db19f8a1716d7d06421a5f4f
ms2138 <m38371@gmail.com>
2022-08-27 19:21:13 -0700

    Add file and user_id properties.  Add initialize method.  Include Model, Conversion, and Validations classes

--- app/models/transaction_import.rb
+++ app/models/transaction_import.rb
@@ -1,2 +1,12 @@
 class TransactionImport
+  include ActiveModel::Model
+  include ActiveModel::Conversion
+  include ActiveModel::Validations
+
+
+  attr_accessor :file, :user_id
+
+  def initialize(attributes = {})
+    attributes.each { |name, value| send("#{name}=", value) }
+  end
 end
