8071546e2c37afbfe1346f93a2b38c7bc686428e
ms2138 <m38371@gmail.com>
2022-09-01 20:11:53 -0700

    Add functionality to TransactionImportsController to allow the creation of new Transactions from uploaded files

--- app/controllers/transaction_imports_controller.rb
+++ app/controllers/transaction_imports_controller.rb
@@ -1,4 +1,16 @@
 class TransactionImportsController < ApplicationController
   def new
+    @transaction_import = TransactionImport.new
+  end
+
+  def create
+    @transaction_import = TransactionImport.new(params[:transaction_import])
+    respond_to do |format|
+      if @transaction_import.save
+        format.html { redirect_to transactions_path, notice: "Imported successfully." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
   end
 end
