8f35ac62eb41cc8e6abc61c009f42de196611cd3
ms2138 <m38371@gmail.com>
2022-09-04 15:52:42 -0700

    Remove edit, form partial, and new views from transactions.  Remove destroy, new, and create action from TransactionsController.  Update transactions routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :transaction_imports, only: [:new, :create]
   
-  resources :transactions
+  resources :transactions, only: [:index, :show, :edit, :update]
   resources :categories
   devise_for :users
 
