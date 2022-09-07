f73b7d3e0d5cf0cfda00610fb246211356255fd6
ms2138 <m38371@gmail.com>
2022-09-06 21:55:46 -0700

    Add user and category seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+20.times do |i|
+  email = "jdoe#{i}@example.com"
+  password = "hello12"
+  @user = User.create!(email: email, password: password, password_confirmation: password)
+end
+
+@restaurant = Category.create!(name: "Restaurants")
+@miscellaneous = Category.create!(name: "Miscellaneous")
+@other = Category.create!(name: "Other")
