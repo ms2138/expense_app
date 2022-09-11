e8684617f6f6066a2b1c555e2bc857fe5822f9ef
ms2138 <m38371@gmail.com>
2022-09-10 23:46:56 -0700

    Add connect and disconnect to setup and destroy chart

--- app/javascript/controllers/chart_controller.js
+++ app/javascript/controllers/chart_controller.js
@@ -1,6 +1,23 @@
+//
+// https://github.com/stimulus-components/stimulus-chartjs
+//
 import { Controller } from "@hotwired/stimulus";
 import Chart from 'chart.js/auto';
 
 export default class extends Controller {
+  connect() {
+    const element = this.hasCanvasTarget ? this.canvasTarget : this.element
 
+    this.chart = new Chart(element.getContext('2d'), {
+      type: this.type || 'line',
+      data: this.data,
+      options: this.options,
+      plugins: this.plugins
+    })
+  }
+
+  disconnect () {
+    this.chart.destroy()
+    this.chart = undefined
+  }
 }
