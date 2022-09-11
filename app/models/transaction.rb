faa8bff18e0e35a66860f54bda997644011755ff
ms2138 <m38371@gmail.com>
2022-09-11 14:20:07 -0700

    Add toDate, current_month, and chart_data_for_month methods to Transaction

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,25 @@
 class Transaction < ApplicationRecord
   belongs_to :category
   belongs_to :user
+
+  def self.chart_data_for_month(month)
+    toDate = toDate(month)
+    select("transactions.amount, categories.name")
+    .where(posted_at: fromDate..toDate)
+    .joins(:category)
+    .group(:name).sum(:amount)
+  end
+
+  def self.current_month(month)
+    toDate = toDate(month)
+    where(posted_at: fromDate..toDate)
+  end
+
+  private
+  
+  def toDate(month)
+    year = Time.now.year
+    fromDate = Time.new(year, month)
+    toDate = Time.new(year, month, fromDate.end_of_month.day)
+  end
 end
