b4ec943fad9080295568d40464e9bd630a034352
ms2138 <m38371@gmail.com>
2022-09-16 22:53:15 -0700

    Remove toDate method from Transaction.  Add year parameter to chart_data_for_month and current_month.  Remove call to toDate in chart_data_for_month and current_month

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,24 +2,14 @@ class Transaction < ApplicationRecord
   belongs_to :category
   belongs_to :user
 
-  def self.chart_data_for_month(month)
-    toDate = toDate(month)
+  def self.chart_data_for_month(month, year)
     select("transactions.amount, categories.name")
-    .where(posted_at: fromDate..toDate)
+    .where(posted_at: posted_at: Time.new(year, month)..Time.new(year, month, Time.days_in_month(month))
     .joins(:category)
     .group(:name).sum(:amount)
   end
 
-  def self.current_month(month)
-    toDate = toDate(month)
-    where(posted_at: fromDate..toDate)
-  end
-
-  private
-  
-  def toDate(month)
-    year = Time.now.year
-    fromDate = Time.new(year, month)
-    toDate = Time.new(year, month, fromDate.end_of_month.day)
+  def self.current_month(month, year)
+    where(posted_at: posted_at: Time.new(year, month)..Time.new(year, month, Time.days_in_month(month))
   end
 end
