c4dbf05a44336736d4a4a39e852c4e4f95551390
ms2138 <m38371@gmail.com>
2022-09-22 22:18:21 -0700

    Add setup code for pundit authorization in ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,16 @@ class ApplicationController < ActionController::Base
   include Pagy::Backend
   before_action :authenticate_user!
 
+  include Pundit::Authorization
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   private
   
   def after_sign_out_path_for(resource_or_scope)
     new_user_session_path
+  end
+
+  def user_not_authorized
+    redirect_to root_path, alert: 'You are not authorized to perform this action.'
   end
 end
