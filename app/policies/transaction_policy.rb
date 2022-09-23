b655bf9dc734f0b5a65ce701909d3cd24993aa5d
ms2138 <m38371@gmail.com>
2022-09-22 22:38:37 -0700

    Add override for resolve method to TransactionPolicy.  Add policies for show, edit, and update

--- app/policies/transaction_policy.rb
+++ app/policies/transaction_policy.rb
@@ -1,3 +1,19 @@
 class TransactionPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.where(user_id: user.id)
+    end
+  end
 
+  def show?
+    user == record.user
+  end
+
+  def edit?
+    update?
+  end
+
+  def update?
+    user == record.user
+  end
 end
