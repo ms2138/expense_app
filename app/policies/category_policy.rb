c753bc879de70f851c93151a79dd639c95ff6339
ms2138 <m38371@gmail.com>
2022-09-25 19:59:18 -0700

    Add Category policies for index, show, edit, and update, create, new and destroy

--- app/policies/category_policy.rb
+++ app/policies/category_policy.rb
@@ -4,4 +4,28 @@ class CategoryPolicy < ApplicationPolicy
       scope.where(user_id: user.id)
     end
   end
+
+  def show?
+    user.present?
+  end
+
+  def create?
+    user.present?
+  end
+
+  def new?
+    create?
+  end
+
+  def edit?
+    update?
+  end
+
+  def update?
+    user.present? && user == record.user
+  end
+
+  def destroy?
+    user.present? && user == record.user
+  end
 end
