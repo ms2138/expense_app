24b9036af62d09811d1fb5c2353627f8a5ec4ade
ms2138 <m38371@gmail.com>
2022-10-01 20:12:27 -0700

    Add dependent destroy to transaction relationship.  Add ordered scope to Category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,8 @@
 class Category < ApplicationRecord
   belongs_to :user
-  has_many :transactions
+  has_many :transactions, dependent: :destroy
+
+  scope :ordered, -> { order(created_at: :asc) }
 
   validates :name, presence: true, length: { minimum: 1, maximum: 90 }
 end
