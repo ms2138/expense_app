b952b2072414135bf8cd610e499fa4de77a2aaae
ms2138 <m38371@gmail.com>
2022-10-06 23:17:48 -0700

    Add ordered scope to Transaction

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,6 +2,8 @@ class Transaction < ApplicationRecord
   belongs_to :category
   belongs_to :user
 
+  scope :ordered, -> { order(posted_at: :asc) }
+
   def self.chart_data_for_month(month, year)
     select("transactions.amount, categories.name")
     .where(posted_at: Time.new(year, month)..Time.new(year, month, Time.days_in_month(month)))
