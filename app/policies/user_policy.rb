2b0e56bd03fa8ba1aab56b7736ce4d8ce3955dc8
ms2138 <m38371@gmail.com>
2022-10-09 19:02:14 -0700

    Add user show action policy to UserPolicy

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,8 +1,5 @@
 class UserPolicy < ApplicationPolicy
-  class Scope < Scope
-    # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+  def show?
+    user.present? && user.id == record.id
   end
 end
