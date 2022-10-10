d8b442901f8d287a4f655ecb8dfe68b152c8cd71
ms2138 <m38371@gmail.com>
2022-10-09 19:05:48 -0700

    Update layout for user show view.  Add set_user method, before and after actions to UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[ show ]
+  after_action :verify_authorized, except: :index
+
   def show
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = authorize User.find(params[:id])
+    redirect_to root_url if @user.nil?
   end
 end
