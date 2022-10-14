cab9b2602f0f8f83ee010b10de6863013baf4ea2
ms2138 <m38371@gmail.com>
2022-10-13 23:39:43 -0700

    Add checked funtion to checkbox_select_controller

--- app/javascript/controllers/checkbox_select_controller.js
+++ app/javascript/controllers/checkbox_select_controller.js
@@ -1,5 +1,15 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-
+  static targets = ["submit", "child"];
+  
+  checked() {
+    if (this.childTargets.map(x => x.checked).includes(true)) {
+      if (this.submitTarget.classList.contains("hidden")) {
+        this.submitTarget.classList.remove('hidden')
+      }
+    } else {
+      this.submitTarget.classList.add('hidden')
+    }
+  }
 }
