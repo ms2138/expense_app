9ea4e06abbd422a4fe8471ead45cf26d8ad06d53
ms2138 <m38371@gmail.com>
2022-10-15 19:13:27 -0700

    Add destroy_multiple route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :transaction_imports, only: [:new, :create]
-  resources :transactions, only: [:index, :show, :edit, :update]
+  resources :transactions, only: [:index, :show, :edit, :update] do
+    collection do
+      delete 'destroy_multiple'
+    end
+  end
   
   resources :users, only: [:show], shallow: true do
     resources :categories
